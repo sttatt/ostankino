f2f52f9eb5311711a6d8ff41760df26fd1d54969
Andrey Chentsov <sttatt@bk.ru>
2021-05-10 13:11:04 +0300

    init apps

---
+++ webapp/analyse_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AnalyseAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'analyse_app'
---
+++ webapp/parse_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ParseAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'parse_app'
